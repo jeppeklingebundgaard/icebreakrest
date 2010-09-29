65d453a47eeb67597e3d0c477da460b717cf31a6
niels.liisberg@gmail.com <niels.liisberg@gmail.com@07541d2d-8d22-7c45-ee42-a23c645956f4>
2010-09-29 15:05:10 +0000

    Ver 1.2 ready

--- IceBreakRestServer/src/IceBreakRestServer/IceBreakRestServer.java
+++ IceBreakRestServer/src/IceBreakRestServer/IceBreakRestServer.java
@@ -14,7 +14,7 @@
 /*    See the License for the specific language governing permissions and                   */
 /*    limitations under the License.                                                        */
 /*                                                                                          */
-/*    Design - Niels Liisberg                                                                                          */
+/*    Design - Niels Liisberg                                                               */
 /*                                                                                          */
 /* ---------------------------------------------------------------------------------------- */
 package IceBreakRestServer;
@@ -29,6 +29,55 @@ import java.util.Properties;
 import java.net.URL.*;
 import java.net.URLDecoder;
 
+/**
+ * Super tiny HTTP serverside protocol for monolitic RESTservice applications
+ * Simply drop the IceBreakRestServer jar file in your project ( classpath) and you are golden.
+ *
+ * A simple server looks like this:
+ *
+ *
+ * <pre>
+ * {@code
+ * // Drop this jar-file into you project
+ * import IceBreakRestServer.*;
+ * import java.io.IOException;
+ * public class Simple {
+ *
+ *   public static void main(String[] args) {
+ *
+ *     // Declare the IceBreak HTTP REST server class
+ *     IceBreakRestServer rest;
+ *
+ *
+ *     try {
+ *
+ *       // Instantiate it once
+ *       rest  = new IceBreakRestServer();
+ *
+ *       while (true) {
+ *
+ *         // Now wait for any HTTP request
+ *         // the "config.properties" file contains the port we are listening on
+ *         rest.getHttpRequest();
+ *
+ *         // If we reach this point, we have received a request
+ *         // now we can pull out the parameters from the query-string
+ *         // if not found we return the default "N/A"
+ *         String name = rest.getQuery("name", "N/A");
+ *
+ *         // we can now produce the response back to the client.
+ *         // That might be XML, HTML, JSON or just plain text like here:
+ *         rest.write("Hello world - the 'name' parameter is: " + name );
+ *       }
+ *     } catch (IOException ex) {
+ *       System.out.println(ex.getMessage());
+ *     }
+ *   }
+ * }
+ * }
+ * </pre>
+ */
+
 
 public class IceBreakRestServer {
 
@@ -42,14 +91,25 @@ public class IceBreakRestServer {
   private int Port ;
   private int Queue ;
   private InputStream in;
-  public  String request;
-  public  String payload;
-  public  String method ;
-  public  String queryStr;
-  public  String resource;
-  public  String httpVer;
 
+  /** This is the complete querysting including the resource. Just as you write it in your browser - you have to URL decode it or rather use getQuery to get paramter */
+  public  String request;
+  /** This is the contents sent by a POST  */
+  public  String payload;
+  /** This is the request type GET, POST, HEAD - your application have to responde coretly to this ( ore simply ignore it */
+  public  String method ;
+  /** This is the complete querysting after the resource as you write it in your browser - you have to URL decode it or rather use getQuery to get paramter */
+  public  String queryStr;
+  /** This is the name of the resource to run or get i.e. http://x/myApp.aspx/p1=abc it will return /myApp.aspx  */
+  public  String resource;
+  /** This is the version of the HTTP protocol requested   */
+  public  String httpVer;
+  /** Set this to true to get some system.out.print */
+  public  Boolean debug = false; 
+
+  /** This is the HTTP headers in the request. Use normal "Map" methods  */
   public  Map<String, String> header = new HashMap<String, String>();
+  /** This is the HTTP quesystring parameters as map. Use normal "Map" methods or getQuery() method  */
   public  Map<String, String> parms  = new HashMap<String, String>();
 
   private void loadProps ( ) {
@@ -64,36 +124,79 @@ public class IceBreakRestServer {
       // ex.printStackTrace();
     }
   }
-
+  /**
+	 * Contructor, returns an instance of the rest server
+	 */
   public IceBreakRestServer() {
     loadProps ();
   }
 
+  /**
+	 * Set the contents type of the HTTP contens. It has to conform
+   * the mime type. By default it has the value of "text/plain; charset=utf-8"
+	 * @param contents type string to set
+	 */
   public void setContentType(String s) {
     ContentType = s;
   }
+  /**
+	 * Set the status of HTTP contens.
+   * @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html">HTTP status codes</a>
+	 * @param status string . by default the is "200 OK"
+	 */
   public void setStatus(String s) {
     Status = s;
   }
 
+  /**
+	 * Set the TCP/IP port that you server is listening on. This is by default port 65000 and you can
+   * set this value in the config.prperties file. Or you can set it programatically here but before issuing
+   * a "getRequest()".
+   * @param port TCP/IP port to listen on
+	 */
   public void setPort(int port) {
     Port = port;
   }
 
+  /**
+	 * Set the TCP/IP queue depth for your HTTP server . This is by default port 10 and you can
+   * set this value in the config.prperties file. Or you can set it programatically here but before issuing
+   * a "getRequest()".
+   * @param port TCP/IP port to listen on
+	 */
   public  void setQueue(int queue) {
      Queue = queue;
   }
 
+  /**
+	 * Returns the parameter from the querystring with the name of "key". if the querystring
+   * parameter was not fount it will return the default paramter
+   * Note: key is case sensitive!!
+   * @param Key - to return value for in the querystring
+   * @param Default - when key is not found this wil be the default value
+   * @return value of the querystring parameter
+	 */
   public  String getQuery(String Key , String Default) {
      String temp = parms.get(Key);
      if (temp == null) return Default;
      return temp;
   }
 
+  /**
+	 * Returns the parameter from the querystring with the name of "key". if the querystring
+   * parameter was not fount it will <code>null</code>
+   * Note: key is case sensitive!!
+   * @param Key - to return value for in the querystring
+   * @return value of the querystring parameter
+	 */
   public  String getQuery(String Key) {
      return parms.get(Key);
   }
 
+  /**
+	 * Just return a simple string with current timestamp in hh:mm:ss format
+   * @return current time in hh:mm:ss format
+	 */
   public String now () {
     String s;
     Format formatter;
@@ -103,7 +206,7 @@ public class IceBreakRestServer {
     return s;
   }
 
-  public static Map<String, String> getQueryMap(String query) {  
+  private static Map<String, String> getQueryMap(String query) {
      String[] params = query.split("&");  
      Map<String, String> map = new HashMap<String, String>();  
      for (String param : params)  {  
@@ -145,7 +248,7 @@ public class IceBreakRestServer {
 
  private void unpackRequest() throws IOException {
 
-    byte buf [] = new byte[4096];
+    byte buf [] = new byte[32768];
     in = connection.getInputStream();
     int read = in.read(buf);
     int len =0, pos =0, eol=0;
@@ -189,17 +292,19 @@ public class IceBreakRestServer {
     }
 
     // this is only for debugging
-    System.out.println("resource: " + request);
-    System.out.println("method: " + method);
-    System.out.println("resource: " + resource);
-    System.out.println("queryStr: " + queryStr);
-    System.out.println("httpVer: " + httpVer);
-    System.out.println("header  : " + header   );
-    System.out.println("parms : " + parms  );
+    if (debug) {
+      System.out.println("resource: " + request);
+      System.out.println("method: " + method);
+      System.out.println("resource: " + resource);
+      System.out.println("queryStr: " + queryStr);
+      System.out.println("httpVer: " + httpVer);
+      System.out.println("header  : " + header   );
+      System.out.println("parms : " + parms  );
+    }
 
   }
 
-  void sendResponse () {
+  private void sendResponse () {
 
     pw.print("HTTP/1.1 " + Status + "\r\n" +
              "Connection: Keep-Alive\r\n" +
@@ -213,6 +318,10 @@ public class IceBreakRestServer {
     pw.flush();
   }
 
+  /**
+	 * This waits for the next HTTP request from the client
+   *
+	 */
   public void getHttpRequest () throws IOException {
     if (providerSocket == null) {
       providerSocket = new ServerSocket(Port , Queue);
@@ -228,10 +337,18 @@ public class IceBreakRestServer {
     doFlush = true;
   }
 
+  /**
+	 * write a string back a tring to the client. The complete result will be
+   * send back to the client when you issue a "flush" or do the next "getHttpRequest()"
+   * @param String - to send back to the client
+	 */
   public void write(String s) {
     resp.append(s);
   }
 
+  /**
+	 * send back the complete response to the client. Now we are ready to wait for the next request by issue a "getHttpRequest()"
+	 */
   public void flush() throws IOException {
     sendResponse ();
     connection.close();
